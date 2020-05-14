76d49c17085fb2f5aafd235efc0d9808c311fe00
Nazneen Nisar Ahmed Tamboli <ntamboli@seattleu.edu>
2020-05-14 13:54:33 -0700

    Green ground added to scene

--- Game-VS/src/GameData.h
+++ Game-VS/src/GameData.h
@@ -22,7 +22,7 @@ const char* OBJ_FOUNTAIN   = "res/objects/fountains/fountainOBJ/fountainOBJ.obj"
 const char* OBJ_BACKPACK   = "res/LearnOpenGL/objects/backpack/backpack.obj";
 const char* OBJ_HOUSE      = "res/objects/Monster House/Monster House.obj";
 const char* OBJ_ROCK       = "res/objects/ground/rock/rock.obj";
-const char* OBJ_GROUND     = "res/objects/ground/groundModel/ground.obj";
+const char* OBJ_GROUND     = "res/objects/greenground/ground.obj";
 const char* OBJ_TREE       = "res/objects/flora/trees/fir/fir.obj";
 const char* OBJ_HARP       = "res/objects/instruments/harp/3d-model.obj";
 const char* OBJ_STONEFLOOR = "res/objects/ground/stone-ground/test/StoneFloor_Sample.obj";
@@ -31,7 +31,7 @@ const char* OBJ_BIRDS      = "res/objects/animals/birds/two-songbirds/lowpoly_bi
 // game object starting positions
 glm::vec3 tranBackpack(0.5f, -1.2f, 0.0f),    scaleBackpack(0.5f),  rotBackpack(0.0f);
 glm::vec3 tranHouse(-15.0f, -8.0f, -20.0f),   scaleHouse(0.42f),    rotHouse(0.0f, 70.0f, 0.0f);
-glm::vec3 tranGround(50.0f, -8.0f, -200.0f),  scaleGround(20.0f),   rotGround(90.0f, 0.0f, 0.0f);
+glm::vec3 tranGround(50.0f, -8.0f, -200.0f),  scaleGround(40.0f),   rotGround(0.0f, 180.0f, 180.0f);
 glm::vec3 tranTreeFir(0.0f, -8.0f, -10.0f),   scaleTreeFir(0.008f), rotTreeFir(0.0f);
 glm::vec3 tranRock(-8.0f, -8.0f, -15.0f),     scaleRock(0.34f),     rotRock(0.0f);
 glm::vec3 tranFountain(-10.0f, -7.9f, -5.0f), scaleFountain(0.07f), rotFountain(0.0f);
